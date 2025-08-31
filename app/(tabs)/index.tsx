a6ce0fc9cfddc741458066b9144ad23bd8adbff9
Pedro Fabricio <pedrofabriciodev@gmail.com>
2025-08-31 17:09:55 -0300

    feat: add the home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,198 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { 
+  SafeAreaView,
+  View, 
+  Text, 
+  TouchableOpacity, 
+  StyleSheet,
+  Platform,
+  StatusBar 
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import { MaterialIcons } from "@expo/vector-icons/";
+
+import { useRouter } from "expo-router";
+
+
+const Dashboard = () => {
+    const insets = useSafeAreaInsets();
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={[styles.safeArea, {paddingTop: insets.top},]}>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>Bem Vindo</Text>
+                    <TouchableOpacity style={{position:'absolute', right:16}}><MaterialIcons name="settings" size={24}/></TouchableOpacity>
+                </View>
+
+                <View style={styles.section}>
+                    <Text style={styles.sectionTitle}>Resumo Mensal</Text>
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly'}}> 
+
+                        <View style={styles.card}>
+                            <Text style={styles.cardLabel}>Total Recebido</Text>
+                            <Text style={styles.cardValue}>R$ 5.500</Text>
+                        </View>
+                        
+                        <View style={styles.card}>
+                            <Text style={styles.cardLabel}>Gastos Totais</Text>
+                            <Text style={styles.cardValue}>R$ 2.200</Text>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={styles.section}>
+                    <Text style={styles.sectionTitle}>Contas A Pagar</Text>
+
+                    <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+
+                        <TouchableOpacity style={styles.miniCard}>
+                            <Text style={styles.miniCardText}>Esta Semana</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.miniCard}>
+                            <Text style={styles.miniCardText}>Próxima Semana</Text>
+                        </TouchableOpacity>
+                        {/* <View style={styles.miniCard}>
+                            <Text style={styles.miniCardText}>Próximo Mês</Text>
+                        </View> */}
+                        <TouchableOpacity style={styles.miniCard}>
+                            <Text style={styles.miniCardText}>Pagos</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+
+                <TouchableOpacity style={styles.addButton} onPress={() => router.push('/addBills')} >
+                    <MaterialIcons name="add" size={32} color={'white'}/>
+                </TouchableOpacity>
+
+
+                {/* <View style={styles.footer}>
+                  <TouchableOpacity style={{alignItems: 'center'}}>
+                      <MaterialIcons name="home" size={32}/> 
+                      <Text>Inicio</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={{alignItems: 'center'}}>
+                          <MaterialIcons name="receipt" size={24} /> 
+                          <Text>Contas</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={{alignItems: 'center'}}>
+                      <MaterialIcons name="insert-chart-outlined" size={24} /> 
+                      <Text>Extrato</Text>
+                  </TouchableOpacity>  
+                </View> */}
+            </View>
+        </SafeAreaView>
+    );
+};
+
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    safeArea:{
+      flex:1,
+  // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  // backgroundColor: '#fff',
+
+    },
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#ffffff',
+    },
+    header: {
+        flexDirection: 'row',
+        marginBottom: 20,
+        alignItems: 'center',
+        position: 'relative',
+    },
+    headerText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+        marginRight: 0
+    },
+    section: {
+        marginBottom: 24
+    },
+    sectionTitle: {
+        fontSize: 22,
+        fontWeight: '600',
+        marginBottom: 12,
+        color: '#2c3e50'
+    },
+    card: {
+        backgroundColor: '#F0F2F5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        shadowRadius: 3,
+        elevation: 2,
+        width: 171,
+        height:110
+
+    },
+    cardLabel: {
+        fontSize: 14,
+        color: '#121217',
+        marginBottom: 4
+    },
+    cardValue: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#121217'
+    },
+    addButton: { 
+        borderRadius: 35, 
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 45,
+        width: 70,
+        height: 70,
+        right: 20,// ou ajuste conforme altura do tabBar
+    },
+    addButtonText: {
+        fontSize: 40,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+
+
+    miniCard: {
+        backgroundColor: '#F0F2F5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        shadowRadius: 3,
+        elevation: 2,
+        width: '30%',
+        height: 32,
+    },
+    miniCardText: {
+        fontSize: 14,
+        color: '#121217'
+    },
+
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#FFFFFF',
+        padding: 16,
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderTopColor: '#F0F2F5',
+        height: 73
   },
 });
+
+export default Dashboard;
