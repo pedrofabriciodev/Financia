58a71e51b50cb88e51b76513709b00693248aadf
Pedro Fabricio <pedrofabriciodev@gmail.com>
2025-08-31 17:12:03 -0300

    feat: update the navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,60 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+
+import { MaterialIcons } from "@expo/vector-icons/";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+  screenOptions={{
+    tabBarActiveTintColor: '#121217', // Cor do texto/ícone ativo
+    tabBarInactiveTintColor: '#637087', // Cor do texto/ícone inativo
+    headerShown: false,
+    
+    tabBarStyle: {
+      borderTopWidth: 1,
+      borderBottomWidth: 1    ,
+      elevation: 0,
+      borderColor:   '#F0F2F5',  
+      backgroundColor: '#FFFFFF',
+      height: 110,
+      justifyContent: 'space-evenly'
+    },
+    
+    tabBarLabelStyle: {
+      fontSize: 12,
+      marginTop: 4
+    },
+    tabBarIconStyle: {
+      marginTop: 0
+    }
+  }}
+>
+  <Tabs.Screen
+    name="index"
+    options={{
+      title: 'Inicio',
+      tabBarIcon: ({ color }) => <MaterialIcons size={32} name="home" color={color} />,
+
+    }}
+  />
+  <Tabs.Screen
+    name="bill"
+    options={{
+      title: 'Contas',
+      tabBarIcon: ({ color }) => <MaterialIcons size={24} name="receipt" color={color} />,
+    }}
+  />
+  {/* <Tabs.Screen
+    name="extract"
+    options={{
+      title: 'Extrato',
+      tabBarIcon: ({ color }) => <MaterialIcons size={24} name="insert-chart-outlined" color={color} />,
+    }}
+  /> */}
+</Tabs>
+
   );
 }
