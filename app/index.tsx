438862801f1f6c8c71b5525c4a156ca661567840
Pedro Fabricio <pedrofabriciodev@gmail.com>
2025-09-17 20:27:47 -0300

    feat: add index screen

---
+++ app/index.tsx
@@ -0,0 +1,130 @@
+import { router } from "expo-router";
+import { View, StyleSheet, Image, Dimensions, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const Start = () => {
+   const insets = useSafeAreaInsets();
+
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height; 
+    console.log(screenWidth)
+    console.log(screenHeight)
+
+return (
+  <SafeAreaView style={styles.safeArea} edges={['top']}>
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/firstScreen.png')}
+        style={{
+          width: screenWidth,
+          height: screenHeight * 0.4, 
+        }}
+        resizeMode="cover"
+      />
+
+      <View style={{marginBottom:8, marginTop:16}}>
+        <Text style={styles.textMain}>Bem-vindo à sua jornada financeira</Text>
+      </View>
+
+      <View>
+        <Text style={styles.textSecondary}>
+          Assuma o controle de suas finanças e alcance seus objetivos com facilidade.
+        </Text>
+      </View>
+
+      <TouchableOpacity style={styles.startButton} onPress={() => router.replace('/(tabs)/home')}>
+        <Text style={{color:'#FFFFFF'}}>Comece</Text>
+      </TouchableOpacity>
+    
+
+
+    </View>
+  </SafeAreaView>
+);
+}
+
+
+const styles = StyleSheet.create({
+    safeArea:{
+        flex:1,
+        paddingTop:0
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+    textMain: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#121217',
+        textAlign:'center',
+
+    },
+    textSecondary:{
+      fontSize: 16,
+      fontWeight: 'regular',
+      color: '#121217',
+      textAlign:'center',
+
+    },
+    input: {
+       
+        borderRadius: 8,
+        width: 358,
+        height: 56,
+        color: 'black',
+        backgroundColor: '#F0F2F5',
+        marginBottom: 20
+
+      },
+      
+    header: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginBottom: 40,
+        alignItems: 'center',
+    },
+    headerText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+        marginRight: 0
+    },
+    startButton: { 
+        borderRadius: 8,  
+        backgroundColor: '#2B6BE3',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 80,
+        width: 358,
+        height: 48,
+        alignSelf: 'center'
+    },
+
+    addButtonText: {
+        fontSize: 40,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#FFFFFF',
+        padding: 16,
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderTopColor: '#F0F2F5',
+        height: 73,
+  },
+});
+
+export default Start;
